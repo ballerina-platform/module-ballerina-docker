5528c2f66618132ad71059fd508dbcb18b667115
anuruddhal <anuruddhal@wso2.com>
2018-04-11 13:31:16 +0530

    Update ballerina version

--- src/main/java/org/ballerinax/docker/DockerArtifactHandler.java
+++ src/main/java/org/ballerinax/docker/DockerArtifactHandler.java
@@ -81,13 +81,13 @@ public class DockerArtifactHandler {
 
                     @Override
                     public void onError(String message) {
-                        dockerError.setErrorMsg("error building docker image: " + message);
+                        dockerError.setErrorMsg("Unable to build docker image: " + message);
                         buildDone.countDown();
                     }
 
                     @Override
                     public void onError(Throwable t) {
-                        dockerError.setErrorMsg("error building docker image: " + t.getMessage());
+                        dockerError.setErrorMsg("Unable to build docker image: " + t.getMessage());
                         buildDone.countDown();
                     }
 
@@ -137,13 +137,13 @@ public class DockerArtifactHandler {
                     @Override
                     public void onError(String message) {
                         pushDone.countDown();
-                        dockerError.setErrorMsg("error pushing docker image: " + message);
+                        dockerError.setErrorMsg("Unable to push docker image: " + message);
                     }
 
                     @Override
                     public void onError(Throwable t) {
                         pushDone.countDown();
-                        dockerError.setErrorMsg("error pushing docker image: " + t.getMessage());
+                        dockerError.setErrorMsg("Unable to push docker image: " + t.getMessage());
                     }
 
                     @Override

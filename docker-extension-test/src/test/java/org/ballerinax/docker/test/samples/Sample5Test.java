8cf0a2218ae1e08f1f640a1871676a074b371ddb
Joseph Fonseka <hevayo@gmail.com>
2019-09-07 17:54:18 +0530

    Fix docker config passing

--- docker-extension-test/src/test/java/org/ballerinax/docker/test/samples/Sample5Test.java
+++ docker-extension-test/src/test/java/org/ballerinax/docker/test/samples/Sample5Test.java
@@ -82,7 +82,7 @@ public class Sample5Test extends SampleTest {
     public void validateDockerImage() throws DockerTestException {
         Assert.assertEquals(getCommand(this.dockerImage).toString(), "[/bin/sh, -c, java -jar " +
                                                                      "hello_config_file.jar " +
-                                                                     "--b7a.config=" +
+                                                                     "--b7a.config.file=" +
                                                                      "/home/ballerina/conf/ballerina.conf]");
         List<String> ports = getExposedPorts(this.dockerImage);
         Assert.assertEquals(ports.size(), 1);

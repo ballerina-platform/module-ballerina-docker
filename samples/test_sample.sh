778b5a63be2210a0151fb91ee374c72dd99e4f1d
anuruddhal <anuruddhal@wso2.com>
2019-09-09 12:25:33 +0530

    Fix sample3 syntax

--- samples/test_sample.sh
+++ samples/test_sample.sh
@@ -26,7 +26,7 @@ do
 		ballerina build hello_world_docker.bal
 		dockerId=$(docker run -d -p 9090:9090 hello_world_docker:latest)
 		sleep 2
-		curl http://localhost:9090/HelloWorld/sayHello
+		curl http://localhost:9090/helloWorld/sayHello
 		docker kill $dockerId
 	fi
 
@@ -34,11 +34,13 @@ do
 		ballerina build hello_world_docker.bal
 		dockerId=$(docker run -d -p 9090:9090 docker.abc.com/helloworld:v1.0)
 		sleep 2
-		curl https://localhost:9090/HelloWorld/sayHello -k
+		curl https://localhost:9090/helloWorld/sayHello -k
 		docker kill $dockerId
 	fi
 
 	if [[ number -eq 3 ]]; then
+	  export DOCKER_USERNAME=ballerina
+    export DOCKER_PASSWORD=ballerina
 		ballerina build docker_push_sample.bal
 		dockerId=$(docker run -d -p 9090:9090 index.docker.io/$DOCKER_USERNAME/helloworld-push:v2.0.0)
 		sleep 2

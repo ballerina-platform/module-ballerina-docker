57a587cc5a4f6b500ee9c2327b6b75c4ae8a2113
anuruddhal <anuruddhal@wso2.com>
2021-03-10 14:11:03 +0530

    Add Docker Generator tests

--- docker-generator/src/test/java/org/ballerinax/docker/generator/test/DockerGeneratorTests.java
+++ docker-generator/src/test/java/org/ballerinax/docker/generator/test/DockerGeneratorTests.java
@@ -18,18 +18,38 @@
 
 package org.ballerinax.docker.generator.test;
 
+import org.apache.commons.io.FileUtils;
+import org.ballerinalang.model.elements.PackageID;
+import org.ballerinax.docker.generator.DockerArtifactHandler;
 import org.ballerinax.docker.generator.exceptions.DockerGenException;
+import org.ballerinax.docker.generator.models.CopyFileModel;
+import org.ballerinax.docker.generator.models.DockerModel;
+import org.ballerinax.docker.generator.test.utils.DockerTestUtils;
 import org.ballerinax.docker.generator.utils.DockerImageName;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import org.wso2.ballerinalang.compiler.util.Name;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Docker generator tests.
  */
 public class DockerGeneratorTests {
 
-    private final Logger log = LoggerFactory.getLogger(DockerGeneratorTests.class);
+    private static final Path SOURCE_DIR_PATH = Paths.get("src", "test", "resources");
+    private static final String DOCKER_IMAGE = "anuruddhal/test-image:v1";
+    private final PrintStream out = System.out;
 
     @Test(expectedExceptions = DockerGenException.class,
             expectedExceptionsMessageRegExp = "given docker name 'dockerName:latest' is invalid: image name " +
@@ -38,4 +58,59 @@ public class DockerGeneratorTests {
     public void invalidDockerImageNameTest() throws DockerGenException {
         DockerImageName.validate("dockerName:latest");
     }
+
+    @Test
+    public void buildDockerImageTest() throws DockerGenException, IOException {
+        DockerModel dockerModel = new DockerModel();
+        dockerModel.setName("test-image");
+        dockerModel.setRegistry("anuruddhal");
+        dockerModel.setTag("v1");
+        dockerModel.setJarFileName("hello.jar");
+        dockerModel.setPorts(Collections.singleton(9090));
+        dockerModel.setBuildImage(true);
+        dockerModel.setService(true);
+        DockerArtifactHandler handler = new DockerArtifactHandler(dockerModel);
+        Path jarFilePath = SOURCE_DIR_PATH.resolve("docker-test").resolve("hello.jar");
+        Set<Path> jarFilePaths = getJarFilePaths();
+        PackageID packageID = new PackageID(new Name("wso2"), new Name("bal"), new Name("1.0.0"));
+        dockerModel.setPkgId(packageID);
+        dockerModel.setDependencyJarPaths(jarFilePaths);
+        CopyFileModel copyFileModel = new CopyFileModel();
+        copyFileModel.setSource(SOURCE_DIR_PATH.resolve("conf").resolve("Config.toml").toString());
+        copyFileModel.setTarget("/home/ballerina/conf/");
+        copyFileModel.setBallerinaConf(true);
+        dockerModel.setCopyFiles(Collections.singleton(copyFileModel));
+        Path outputDir = SOURCE_DIR_PATH.resolve("target");
+        Files.createDirectories(outputDir);
+        handler.createArtifacts(out, "\t@kubernetes:Docker \t\t\t", jarFilePath, outputDir);
+    }
+
+    @Test(dependsOnMethods = {"buildDockerImageTest"})
+    public void validateDockerFile() throws IOException {
+        File dockerFile = SOURCE_DIR_PATH.resolve("target").resolve("Dockerfile").toFile();
+        Assert.assertTrue(dockerFile.exists());
+        String dockerFileContent = new String(Files.readAllBytes(dockerFile.toPath()));
+        Assert.assertTrue(dockerFileContent.contains("CMD java -Xdiag -cp \"hello.jar:jars/*\" " +
+                "'wso2/bal/1_0_0/$_init'"));
+        Assert.assertTrue(dockerFileContent.contains("USER ballerina"));
+    }
+
+    @Test(dependsOnMethods = {"buildDockerImageTest"})
+    public void validateDockerImage() {
+        Assert.assertNotNull(DockerTestUtils.getDockerImage(DOCKER_IMAGE));
+        Assert.assertEquals(DockerTestUtils.getExposedPorts(DOCKER_IMAGE).size(), 1);
+        Assert.assertEquals(Objects.requireNonNull(DockerTestUtils.getDockerImage(DOCKER_IMAGE).getConfig()
+                .getEnv()).length, 2);
+    }
+
+    private Set<Path> getJarFilePaths() throws IOException {
+        return Files.list(SOURCE_DIR_PATH.resolve("docker-test")).collect(Collectors.toSet());
+    }
+
+    @AfterClass
+    public void cleanUp() throws IOException {
+        FileUtils.deleteDirectory(SOURCE_DIR_PATH.resolve("target").toFile());
+        DockerTestUtils.deleteDockerImage(DOCKER_IMAGE);
+    }
+
 }
